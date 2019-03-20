6dcfa1dedb7c6f9af97b2f15bd3765ee512d1a49
Jeffrey Villamin <jeffmon@gmail.com>
2019-03-19 15:58:20 -1000

    Use useEffect to fetch json and set data as state, using empty array to only this once

--- src/Joke.js
+++ src/Joke.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
 const Joke = () => {
+  const [joke, setJoke] = useState({});
   useEffect(() => {
     fetch('https://official-joke-api.appspot.com/random_joke')
       .then(response => response.json())
-      .then(json => console.log(json));
-  });
+      .then(json => {
+        setJoke(json);
+      });
+  }, []);
+
+  const { setup, punchline } = joke;
   return (
     <div>
-      <h3>Joke</h3>
+      <h3>Joke of the Session</h3>
+      <p>{setup}</p>
+      <p>
+        <em>{punchline}</em>
+      </p>
     </div>
   );
 };
